d511b8b803e58d162af3ae42d0b795e6e839ddb7
mayconline <mayconline.ti@gmail.com>
2025-05-26 15:56:04 -0300

    ci: remove unused env

--- src/services/env.ts
+++ src/services/env.ts
@@ -1,7 +1,6 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  ANDROID_PACKAGE_NAME: z.string(),
   GOOGLE_PRIVATE_KEY: z.string(),
   GOOGLE_CLIENT_EMAIL: z.string(),
   PORT: z.string(),
