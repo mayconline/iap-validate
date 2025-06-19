8600980595735e9ff686c0772401619b5b4382fe
mayconline <mayconline.ti@gmail.com>
2025-06-19 13:02:53 -0300

    ci: test deploy vercel docs 3

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import { env } from './services';
 import express from 'express';
 import routes from './routes';
-import path from 'node:path';
 
 import swaggerDocument from '../doc/openapi.json';
 import swaggerUi from 'swagger-ui-express';
@@ -10,19 +9,14 @@ import swaggerUi from 'swagger-ui-express';
 const app = express();
 const port = env.PORT || 3000;
 
-// Serve swagger-ui static assets
-app.use(
-  '/api/docs/swagger-ui',
-  express.static(path.join(__dirname, '../node_modules/swagger-ui-dist'), {
-    index: false,
-  })
-);
-
-// Setup Swagger UI
+// Setup Swagger UI with CDN
 const swaggerOptions = {
-  customCssUrl: '/api/docs/swagger-ui/swagger-ui.css',
-  customJs: '/api/docs/swagger-ui/swagger-ui-bundle.js',
-  customfavIcon: '/api/docs/swagger-ui/favicon-32x32.png',
+  customCssUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.19.1/swagger-ui.min.css',
+  customJs: [
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.19.1/swagger-ui-bundle.min.js',
+  ],
+  customfavIcon: 'https://swagger.io/favicon.ico',
 };
 
 app.use(express.json());
