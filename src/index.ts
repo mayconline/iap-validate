99f49885a21d25544e005ebd32ab5590a53e5738
mayconline <mayconline.ti@gmail.com>
2025-06-19 12:59:35 -0300

    ci: test deploy vercel docs 2

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { env } from './services';
 import express from 'express';
 import routes from './routes';
+import path from 'node:path';
 
 import swaggerDocument from '../doc/openapi.json';
 import swaggerUi from 'swagger-ui-express';
@@ -9,9 +10,28 @@ import swaggerUi from 'swagger-ui-express';
 const app = express();
 const port = env.PORT || 3000;
 
+// Serve swagger-ui static assets
+app.use(
+  '/api/docs/swagger-ui',
+  express.static(path.join(__dirname, '../node_modules/swagger-ui-dist'), {
+    index: false,
+  })
+);
+
+// Setup Swagger UI
+const swaggerOptions = {
+  customCssUrl: '/api/docs/swagger-ui/swagger-ui.css',
+  customJs: '/api/docs/swagger-ui/swagger-ui-bundle.js',
+  customfavIcon: '/api/docs/swagger-ui/favicon-32x32.png',
+};
+
 app.use(express.json());
 app.use(routes);
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(
+  '/api/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, swaggerOptions)
+);
 
 app.listen(port, () => {
   console.log('Server is running');
